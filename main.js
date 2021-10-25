accfa25c25dd1286c1572305ae5e679ef593d71d
Maria Cecilia Dos Santos <92831066+maariacecilia@users.noreply.github.com>
2021-10-24 23:13:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -24,11 +24,10 @@ function getGitHubProfileInfos() {
     .then(data => {
       userName.textContent = data.name
       userBio.textContent = data.bio
-      userLink.href = data.html
+      userLink.href = data.html_url
       UserImage.src = data.avatar_url
       userLogin.textContent = data.login
     })
-    .then(data => response.json())
 }
 
 getGitHubProfileInfos()
